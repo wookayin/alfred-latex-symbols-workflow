86bdfd368f781bd5287f392b99f13543688f6254
Lucas Pennati <pennatil@users.noreply.github.com>
2015-09-08 14:08:16 +0200

    Added automatic background color detection

--- workflow/main.rb
+++ workflow/main.rb
@@ -19,6 +19,17 @@ if query.nil? then query = '' end
 query = Regexp.escape(query.downcase).delete('\\')
 query_r = /#{query}/i
 
+# Perform some math to understand if the color is light or dark
+rawColors = ENV['alfred_theme_background']
+splitColors = rawColors.split("rgba(")[1].split(")")[0].split(",")
+redIntensity = splitColors[0].to_i
+greenIntensity = splitColors[1].to_i
+blueIntensity = splitColors[2].to_i
+# Level of brightness. Taken from here: http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
+brightness =  0.241*redIntensity*redIntensity + 0.691*greenIntensity*greenIntensity + 0.068*blueIntensity*blueIntensity
+# Check if theme is light
+isLight = brightness > 130
+
 Alfred.with_friendly_error do |alfred|
   # the matching procedure can be boosted up using efficient algorithms
   # such as trie, Aho-Corasick, etc., but naive ones work too :-)
@@ -35,6 +46,14 @@ Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
   matched_symbols.each do |v|
     uid = v.filename
+    
+    if isLight
+      icon = {:name => "icons/#{uid}.png".downcase, :type => 'file'}
+    else
+      icon = {:name => "icons/#{uid}_white.png".downcase, :type => 'file'}
+    end
+    
+    
     fb.add_item({
       :uid => uid,
       :title => v.command,
@@ -42,7 +61,7 @@ Alfred.with_friendly_error do |alfred|
                     (v.mathmode ? "(math mode)" : "")
                    ].join(' '),
       :arg => v.command,
-      :icon => {:name => "icons/#{uid}.png".downcase, :type => 'file'},
+      :icon => icon,
       :valid => "yes"
     })
   end
