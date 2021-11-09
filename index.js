85f122e0aea6669e0baf0e3b9eea9b1a93e250ef
Jongwook Choi <wookayin@gmail.com>
2021-11-09 15:25:21 -0500

    Rewrite the workflow using alfy (in node.js)
    Good bye ruby ...

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+ * Alfred LaTeX symbols workflow
+ *
+ * Written by Jongwook Choi (@wookayin)
+ */
+
+import alfy from 'alfy';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import crypto from 'crypto';
+
+function md5hash(s) {
+  return crypto.createHash('md5').update(s).digest('hex');
+}
+
+// Collect symbols data.
+var symbols_data = [
+  // {command: ..., fontenc: ..., textmode: ..., mathmode: ...}
+];
+yaml.load(fs.readFileSync("./symbols.yaml")).map(function(s) {
+  if (typeof s == 'string') {
+    symbols_data.push({'command': s});
+  }
+  else { // dict. each key can be one of bothmodes/textmode/mathmode
+    for(const [mode, modeargs] of Object.entries({
+      textmode: {textmode: true, mathmode: false},
+      mathmode: {textmode: false, mathmode: true},
+      bothmodes: {textmode: true, mathmode: true}})) {
+      if (s[mode]) {
+        s[mode].map(t => symbols_data.push({
+          ...{command: t, package: s['package'], fontenc: s['fontenc'] || null},
+          ...modeargs  // override textmode/mathmode with the current context
+        }));
+      }
+    }
+  }
+});
+// Process id and filename.
+symbols_data.map(function(s) {
+  s.id = [
+    s.package ? s.package : 'latex2e',
+    s.fontenc ? s.fontenc : 'OT1',
+    s.command.replace('\\', '_'),
+  ].join('-');
+  s['filename'] = 'symbol' + md5hash(s.id);
+});
+
+
+// Search
+const query = function() {
+  if (!alfy.input) {
+    // console.log(symbols_data.slice(30, 40));  // DEBUG
+    return [];
+  }
+
+  var results = alfy
+    .matches(alfy.input, symbols_data, 'command');
+  //console.log(results);
+
+  return results.map(v => ({
+    title: v.command,
+    subtitle: [
+      v.package ? ("Package " + v.package) : '',
+      v.mathmode ? "(math mode)" : '',
+    ].join(' '),
+    icon: {
+      path: `icons/${v.filename}.png`,
+    },
+    arg: v.command,
+  }));
+};
+
+alfy.output(query());
